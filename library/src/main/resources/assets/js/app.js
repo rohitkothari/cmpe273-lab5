6dd92ae8ae00a2dd6f14b94f2a61bc51b81204db
rohitkothari <rohietkothari@gmail.com>
2014-04-12 16:19:04 -0700

    UI rendered to update call
    Implemented jquery ajax put call and disabled button after book was
    lost.

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,20 @@
 $(":button").click(function() {
 	var isbn = this.id;
 	alert('About to report lost on ISBN ' + isbn);
+	var putUrl = "/library/v1/books/"+ isbn +"?status=lost";
+	
+	var ajaxCall = $.ajax({
+	
+	    url: putUrl,
+	    type: "PUT",
+	    dataType: "json",
+	    success: function(response) {
+	        alert(JSON.stringify(response));
+	        $('#'+isbn+'bookStatus').html("lost");
+	        $('#'+isbn).prop("disabled",true);
+	    }
+		
+	});
 });
+
+
